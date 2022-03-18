e4a221aacaba4c6872f35302d8ebaa127a39ca38
Nicolas Broca <n.broca@gmail.com>
2022-03-18 17:06:50 +1100

    update fontawesome import for react

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import React from 'react'
+
 import Header from './containers/header'
 import Home from './containers/home'
 import About from './containers/about'
+
 import './app.css'
 
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './app'
 import './index.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {
+    faEarthAsia
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faEarthAsia)
+
 ReactDOM.render(
     <App />,
     document.getElementById("app")
