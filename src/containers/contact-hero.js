e98c995e7f9bcf3b023f01546ab2c48dc3f5eb11
Nicolas Broca <n.broca@gmail.com>
2022-03-22 08:25:49 +1100

    update data for projects

--- src/containers/contact-hero.js
+++ src/containers/contact-hero.js
@@ -7,28 +7,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Contacts() {
   return (
     <Hero id="contacts">
-            <Hero.Title>Want to work with me? Let's reach out</Hero.Title>
-            <Hero.ColumnContainer small={true}>
-                <Hero.Column style={{textAlign: 'center'}}>
-                    <Hero.Link hoverClass='email' href='mailto:n.broca@gmail.com'>
-                        <Hero.SubTitle>Email</Hero.SubTitle>
-                        <FontAwesomeIcon icon="fa-solid fa-at" size="6x"/>
-                    </Hero.Link>
-                </Hero.Column>
-                <Hero.Column style={{textAlign: 'center'}}>
-                    <Hero.Link hoverClass='github' href='https://github.com/DrKabum/'>
-                        <Hero.SubTitle>GitHub</Hero.SubTitle>
-                        <FontAwesomeIcon icon="fa-brands fa-github" size="6x"/>
-                    </Hero.Link>
-                </Hero.Column>
-                <Hero.Column style={{textAlign: 'center'}}>
-                    <Hero.Link hoverClass='linkedin' href='https://www.linkedin.com/in/nicolas-broca/'>
-                        <Hero.SubTitle>LinkedIn</Hero.SubTitle>
-                        <FontAwesomeIcon icon="fa-brands fa-linkedin" size="6x"/>
-                    </Hero.Link>
-                </Hero.Column>
-            </Hero.ColumnContainer>
-            <Hero.Text>I'd be happy to share with you more information, answer some question you might have, or just have a chat!</Hero.Text>
+        <Hero.Title>Want to work with me? Let's reach out</Hero.Title>
+        <Hero.ColumnContainer small={true}>
+            <Hero.Column style={{textAlign: 'center'}}>
+                <Hero.Link hoverClass='email' href='mailto:n.broca@gmail.com'>
+                    <Hero.SubTitle>Email</Hero.SubTitle>
+                    <FontAwesomeIcon icon="fa-solid fa-at" size="6x"/>
+                </Hero.Link>
+            </Hero.Column>
+            <Hero.Column style={{textAlign: 'center'}}>
+                <Hero.Link hoverClass='github' href='https://github.com/DrKabum/'>
+                    <Hero.SubTitle>GitHub</Hero.SubTitle>
+                    <FontAwesomeIcon icon="fa-brands fa-github" size="6x"/>
+                </Hero.Link>
+            </Hero.Column>
+            <Hero.Column style={{textAlign: 'center'}}>
+                <Hero.Link hoverClass='linkedin' href='https://www.linkedin.com/in/nicolas-broca/'>
+                    <Hero.SubTitle>LinkedIn</Hero.SubTitle>
+                    <FontAwesomeIcon icon="fa-brands fa-linkedin" size="6x"/>
+                </Hero.Link>
+            </Hero.Column>
+        </Hero.ColumnContainer>
+        <Hero.Text>I'd be happy to share with you more information, answer some question you might have, or just have a chat!</Hero.Text>
     </Hero>
   )
 }
--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,10 +1,15 @@
 let projects = [
     {
-        title: "Lorem ipsum : shkoupidou",
+        title: "Binary clock",
         id: 1,
         link: "https://codepen.io/Kabum/pen/oNZrpyN",
         preview: "https://codepen.io/Kabum/pen/oNZrpyN/image/small.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus sed viverra tellus in hac. Et malesuada fames ac turpis egestas sed. Tellus mauris a diam maecenas sed enim. "
+        description: "This clock looks weird, isn't it? Does it actually display time? Well, yes. But it's binary.",
+        articleParagraphs: [
+            "When I was younger, I used to have a binary watch. I was just caught by the unusual aspect it had (and, I must admit, I loved the question it was raising from the people around me). Unfortunatly, I.. accidently destroyed it at work. Well, it wasn't really the right kind of environment to wear this kind of fragile things, so I should only blame myself.",
+            "Anyway, some time ago, I figured I would make on in CodePen as a practice, and also to try to give life back to my loved watch.",
+            "This Pen is actually quite simple, but it does the job really nicely (the style is simple too, but it matches the screen of my old watch, that was in the same spirit. Just a bunch of numbers and two rows of leds).",
+        ]
     },
     {
         title: "Lorem ipsum : shkoupidou",
