68c588975bd7fb6c2750d3b4a96ed19637263d9f
Nicolas Broca <n.broca@gmail.com>
2022-03-23 09:21:35 +1100

    refactor app.js

--- src/app.js
+++ src/app.js
@@ -5,26 +5,30 @@ import Home from './pages/home'
 import Header from './containers/header'
 import Project from './pages/project'
 
-
 import './app.css'
 import { Hero } from './components'
-
 
 export default function App() {
   return (
     <>
-      <Header/>
-      <main id='home'>
+      <Header />
+      <main id="home">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/projects/:id" element={<Project/>} />
-          <Route path="*" element={
-            <Hero>
-              <Hero.SubContainer>
-              <p>Error 404 : the page requested doesn't exist... And by the way, sorry but this page is a work in progress</p>    
-              </Hero.SubContainer>
-            </Hero>
-          } />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects/:id" element={<Project />} />
+          <Route
+            path="*"
+            element={
+              <Hero>
+                <Hero.SubContainer>
+                  <p>
+                    Error 404 : the page requested doesn't exist... And by the
+                    way, sorry but this page is a work in progress
+                  </p>
+                </Hero.SubContainer>
+              </Hero>
+            }
+          />
         </Routes>
       </main>
     </>
